23edac218d4f156248418eee83d7715974ae40c7
root <root@debian>
2013-07-25 15:23:51 +0400

    562564

--- js/rafoCasteel.js
+++ js/rafoCasteel.js
@@ -2,23 +2,28 @@ function rafoCasteel( Container, image_path )
 {
 	if( Container != "" && image_path != "" )
 	{
+		var i = 0;
+		
 		$( Container ).html("<img id='rc_image' src='"+image_path+"'>");
-		$( Image ).css("display","block");
 		$( Container ).append("<div id='rc_win'></div>");
 		
 		var Image = $("#rc_image");
 		var rcWin = $("#rc_win");
 		
+		$( Image ).css("display","block");
 		
-		var i = 0;
+		
+		// overflow black bg
+		i = 0;
 		var rcWinHTML = "";
 		while( i < 4 )
 		{
 			i++;
-			rcWinHTML = rcWinHTML + "<div id='rc_pd_"+i+"' class='rc_pd'></div>";
+			rcWinHTML = rcWinHTML +
+			"<div id='rc_pd_"+i+"' class='rc_pd'></div>" +
+			"<div id='rc_mv_"+i+"' class='rc_mv'></div>";
 		}
 		$( rcWin ).append( "<div id='rc_pd_main'></div>" + rcWinHTML );
-		
 		
 	}
 }
