93d9de7e48ee1a7cce129088b441b39eeb0a9cfc
root <root@debian>
2013-07-25 14:52:46 +0400

    cmt

--- js/rafoCasteel.js
+++ js/rafoCasteel.js
@@ -1,3 +1,28 @@
+function rafoCasteel( Container, image_path )
+{
+	if( Container != "" && image_path != "" )
+	{
+		$( Container ).html("<img id='rc_image' src='"+image_path+"'>");
+		$( Image ).css("display","block");
+		$( Container ).append("<div id='rc_win'></div>");
+		
+		var Image = $("#rc_image");
+		var rcWin = $("#rc_win");
+		
+		
+		var i = 0;
+		var rcWinHTML = "";
+		while( i < 4 )
+		{
+			i++;
+			rcWinHTML = rcWinHTML + "<div id='rc_pd_"+i+"' class='rc_pd'></div>";
+		}
+		$( rcWin ).append( "<div id='rc_pd_main'></div>" + rcWinHTML );
+		
+		
+	}
+}
+
 $(document).ready(function() {
-	alert(0);
+	rafoCasteel( "#container", "images/1.jpg" );
 });
